7b52aa7a340e06545fe8bca1f7ccbbd263c4e872
philemonmurangiri <murangirilesx890@gmail.com>
2024-03-31 21:02:04 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+  let  input = "I was clicked"
 }
+const input  = document.getElementsByClassName("button")
+input.addingEventListener("button", input )
+
+function addingEventListener() {
+    let input = document.getElementById('button');
+    input.addEventListener('click', function() {
+      console.log('Button clicked!');
+    });
+  }
